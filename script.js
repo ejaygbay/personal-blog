c7453fdbadabe62f8806ef5e4b29588629a9b0f2
Emmanuel Jaygbay <Emmanuel Jaygbay>
2021-01-06 21:35:53 +0000

    Mobile menu completed.

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 const hambugerMenu = (e) => {
   let id = e.target.id;
+  if (id === "line1") {
+    id = e.target.parentElement.id;
+  }
   document.getElementById(id).style = "display: none";
+
+  document.getElementById("side-bar2").style = "display: block";
+
+  document.getElementById("side-bar").style =
+    "width: 70%;transition-timing-function: ease;";
+};
+
+const hideSidebar = (e) => {
+  document.getElementById("side-bar").style = "width: 0%";
+
+  document.getElementById("side-bar2").style = "display: none";
+
+  let hambuger = setTimeout(() => {
+    document.getElementById("hambuger-menu").style = "display: block";
+    clearTimeout(hambuger);
+  }, 200);
 };
