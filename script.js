8f5b228297b194cfd67d22fffe80c4cf6a2ea81b
Emmanuel Jaygbay <Emmanuel Jaygbay>
2021-01-07 08:26:18 +0000

    Page section structure set.

--- script.js
+++ script.js
@@ -20,8 +20,34 @@ const hideSidebar = (e) => {
 
   let hambuger = setTimeout(() => {
     document.getElementById("hambuger-menu").style = "display: block";
-
-    document.getElementById("menu-items").style = "display: none";
     clearTimeout(hambuger);
   }, 200);
+
+  let menu_item_timeout = setTimeout(() => {
+    document.getElementById("menu-items").style = "display: none";
+    clearTimeout(menu_item_timeout);
+  }, 80);
 };
+
+const selectMenu = (e) => {
+  let id = `${e.target.id}-section`;
+  console.log(id);
+  showElement(id);
+};
+
+const showElement = (id) => {
+  let eles = document.querySelectorAll(".section-item");
+
+  eles.forEach((ele) => {
+    if (ele.id !== id) {
+      ele.classList.add("hide");
+    }
+  });
+
+  document.getElementById(id).classList.remove("hide");
+  document.getElementById(id).classList.add("show");
+
+  hideSidebar();
+};
+
+const hideElement = (id) => {};
